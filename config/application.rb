0259e75a67b2db18947594b6c19aab924c7923a0
Marcin Zgórecki <marcin.zgorecki@netguru.com>
2020-12-04 23:13:34 +0100

    Include only important packages

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,21 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require 'sprockets/railtie'
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  action_cable/engine
+  action_text/engine
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
