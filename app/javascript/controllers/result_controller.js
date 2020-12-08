42325ce72cfac4e877536955563c4a8b55f89e64
Marcin Zgórecki <marcin.zgorecki@netguru.com>
2020-12-08 20:41:57 +0100

    Add ResultReflex to handle operation reflexable

---
+++ app/javascript/controllers/result_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from 'stimulus'
+import StimulusReflex from 'stimulus_reflex'
+
+export default class extends Controller {
+  static targets = ['translate', 'wordid', 'strategy']
+
+  connect () {
+    StimulusReflex.register(this)
+  }
+
+  triggerResult() {
+    this.stimulate('ResultReflex#trigger_result', {
+      'translate': this.translateTarget.value,
+      'word_id': this.wordidTarget.value,
+      'strategy': this.strategyTarget.value
+    })
+  }
+}
---
+++ app/reflexes/result_reflex.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class ResultReflex < ApplicationReflex
+  def trigger_result(result)
+    result = result.with_indifferent_access
+    handle_by(result)
+  end
+
+  private
+
+  def handle_by(result)
+    word_object = Word.find_by(id: result[:word_id])
+
+    if check_result(result, word_object)
+      word_object.result.positives =+ 1
+    else
+      word_object.result.negatives =+ 1
+    end
+
+    word_object.result.save
+  end
+
+  def check_result(result, word_object)
+    result[:translate].downcase ==
+      word_object.public_send(result[:strategy]).downcase
+  end
+end
